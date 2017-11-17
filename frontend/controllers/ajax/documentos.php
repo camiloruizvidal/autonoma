8cf6175052d5d0e736a48e891ac0cf0ed1f19566
camiloruizvidal <camiloruizvidal@gmail.com>
2017-11-16 23:32:07 -0500

    #012

--- frontend/controllers/ajax/documentos.php
+++ frontend/controllers/ajax/documentos.php
@@ -10,7 +10,18 @@ FROM
 $Data = conexion::records($sql);
 foreach ($Data as $key => $temp)
 {
-    $temp['iddocumento'] = '<a href="./index.php?r=documento%2Fdownload&amp;id=' . $temp['iddocumento'] . '" title="Descargar" data-pjax="0"><img src="image/descarga.png" width="15" alt=""></a>, <a href="/autonoma/frontend/web/index.php?r=documento%2Fupdate&amp;id=' . $temp['iddocumento'] . '" title="Update" aria-label="Update" data-pjax="0"><span class="glyphicon glyphicon-pencil"></span></a>, <a href="/autonoma/frontend/web/index.php?r=documento%2Fdelete&amp;id=' . $temp['iddocumento'] . '" title="Delete" aria-label="Delete" data-pjax="0" data-confirm="Are you sure you want to delete this item?" data-method="post"><span class="glyphicon glyphicon-trash"></span></a>';
+    $temp['iddocumento'] = '
+            <div class="btn-group" role="group">
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Opciones
+                  <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu">
+                  <li><a href="./index.php?r=documento%2Fdownload&amp;id=' . $temp['iddocumento'] . '" title="Descargar" data-pjax="0"><img src="image/descarga.png" width="15" alt=""> Descargar</a></li>
+                  <li><a href="/autonoma/frontend/web/index.php?r=documento%2Fupdate&amp;id=' . $temp['iddocumento'] . '" title="Update" aria-label="Update" data-pjax="0"><span class="glyphicon glyphicon-pencil"></span> Editar</a></li>
+                  <li><a href="/autonoma/frontend/web/index.php?r=documento%2Fdelete&amp;id=' . $temp['iddocumento'] . '" title="Delete" aria-label="Delete" data-pjax="0" data-confirm="Are you sure you want to delete this item?" data-method="post"><span class="glyphicon glyphicon-trash"></span> Eliminar</a></li>
+                </ul>
+              </div>';
     $Data[$key]          = $temp;
 }
-echo visual::Tabla($Data,array('#','Proyectos','Opciones'),'table_documentos');
+echo visual::Tabla($Data, array('#', 'Proyectos', 'Opciones'), 'table_documentos');
