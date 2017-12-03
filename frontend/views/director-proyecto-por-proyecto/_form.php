2bd8e92ff83908072c8778038dcfc3c43af7801c
camiloruizvidal <camiloruizvidal@gmail.com>
2017-12-03 15:25:15 -0500

    #041

--- frontend/views/director-proyecto-por-proyecto/_form.php
+++ frontend/views/director-proyecto-por-proyecto/_form.php
@@ -12,23 +12,22 @@ use yii\helpers\ArrayHelper;
 ?>
 
 <div class="director-proyecto-por-proyecto-form">
-
-    <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'iddirector_proyecto')->dropDownList(
-        ArrayHelper::map(DirectorProyecto::find()->all(), 'iddirector_proyecto', 'nombre'),
-        ['prompt' => 'seleccione el director']
-      ) ?>
-
-    <?= $form->field($model, 'idproyecto')->dropDownList(
-        ArrayHelper::map(Proyecto::find()->all(), 'idproyecto', 'nombre'),
-        ['prompt' => 'seleccione el Proyecto']
-      ) ?>
-
+    <?php $form = ActiveForm::begin();
+    echo '<pre>';
+    var_dump(DirectorProyecto::findAll($condition));
+    exit;?>
+    <?=
+    $form->field($model, 'iddirector_proyecto')->dropDownList(
+            ArrayHelper::map(DirectorProyecto::find()->all(), 'iddirector_proyecto', 'nombre'), ['prompt' => 'seleccione el director']
+    )
+    ?>
+    <?=
+    $form->field($model, 'idproyecto')->dropDownList(
+            ArrayHelper::map(Proyecto::find()->all(), 'idproyecto', 'nombre'), ['prompt' => 'seleccione el Proyecto']
+    )
+    ?>
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Asignar' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-primary' : 'btn btn-primary']) ?>
+    <?= Html::submitButton($model->isNewRecord ? 'Asignar' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-primary' : 'btn btn-primary']) ?>
     </div>
-
-    <?php ActiveForm::end(); ?>
-
+<?php ActiveForm::end(); ?>
 </div>
--- frontend/views/director-proyecto-por-proyecto/create.php
+++ frontend/views/director-proyecto-por-proyecto/create.php
@@ -2,31 +2,20 @@
 
 use yii\helpers\Html;
 
-
 /* @var $this yii\web\View */
 /* @var $model backend\models\DirectorProyectoPorProyecto */
 
-$this->title = 'Director Por Proyecto';
+$this->title                   = 'Director Por Proyecto';
 $this->params['breadcrumbs'][] = ['label' => 'Director Proyecto Por Proyectos', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<!--
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css"/>
-<script>
-$(function()
-{
-	$('#directorproyectoporproyecto-iddirector_proyecto').select2();
-	$('#directorproyectoporproyecto-idproyecto').select2();
-	
-});
-</script>
--->
 <div class="director-proyecto-por-proyecto-create">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
+    <?=
+    $this->render('_form', [
         'model' => $model,
-    ]) ?>
+    ])
+    ?>
 
 </div>
