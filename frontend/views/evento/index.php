920487985e377650d0af8a7d1ac60962e13cd9ad
camiloruizvidal <camiloruizvidal@gmail.com>
2018-01-06 22:59:20 -0500

    #053

--- frontend/views/evento/index.php
+++ frontend/views/evento/index.php
@@ -14,29 +14,70 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="evento-index">
     <div class="container-fluid">
-        <div class="panel panel-primary">
-            <div class="panel-heading"><?= Html::encode($this->title) ?></div>
-            <div class="panel-body">
-                <script>
-                    $(function ()
-                    {
+        <div class="col-md-6">
+            <div class="panel panel-primary">
+                <div class="panel-heading">Nuevo evento</div>
+                <form id="new_evento">
+                    <div class="panel-body">
+                        <div class="container-fluid">
+                            <div class="col-md-12">
+                                <label>Fecha</label>
+                                <input id="fecha_evento" name="fecha_evento" class="form form-control"/>
+                            </div>
+                            <div class="col-md-12">
+                                <label>Titulo</label>
+                                <input id="titulo" name="titulo" class="form form-control"/>
+                            </div>
+                            <div class="col-md-12">
+                                <label>Descripcion</label>
+                                <textarea rows="8" id="descripcion" name="descripcion" class="form form-control"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-footer">
+                        <button class="btn btn-primary">Guardando</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="panel panel-primary">
+                <div class="panel-heading"><?= Html::encode($this->title) ?></div>
+                <div class="panel-body">
+                    <script>
+                        $(function ()
+                        {
+                            $('#new_evento').submit(function (e)
+                            {
+                                e.preventDefault();
+                                $.ajax({
+                                    url: '',
+                                    data: $('#new_evento').serialize(),
+                                    type: 'POST',
+                                    success: function (e)
+                                    {
+                                        
+                                    }
+                                });
+                            });
+                        });
+                    </script>
 
-                    })
-                </script>
-                <?php
-                Modal::begin([
-                    'header' => '<h4>Cronograma</h4>',
-                    'id'     => 'modal',
-                    'size'   => 'modal-lg',
-                ]);
-                echo "<div id='modalContent'></div>";
-                Modal::end();
-                ?>
-                <?=
-                \yii2fullcalendar\yii2fullcalendar::widget([
-                    'options' => ['lang' => 'es',],
-                    'events'  => $events,]);
-                ?>
+                    <?php
+                    Modal::begin([
+                        'header' => '<h4>Cronograma</h4>',
+                        'id'     => 'modal',
+                        'size'   => 'modal-lg',
+                    ]);
+                    echo "<div id='modalContent'></div>";
+                    Modal::end();
+                    ?>
+                    <?=
+                    \yii2fullcalendar\yii2fullcalendar::widget([
+                        'options' => ['lang' => 'es',],
+                        'events'  => $events,]);
+                    ?>
+                </div>
             </div>
         </div>
     </div>
