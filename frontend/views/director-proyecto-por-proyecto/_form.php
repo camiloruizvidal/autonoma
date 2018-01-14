f180f06327c01e620fa4340a8799248b981cbcf3
camiloruizvidal <camiloruizvidal@gmail.com>
2018-01-14 16:27:10 -0500

    #062

--- frontend/views/director-proyecto-por-proyecto/_form.php
+++ frontend/views/director-proyecto-por-proyecto/_form.php
@@ -9,21 +9,15 @@ use yii\helpers\ArrayHelper;
 /* @var $this yii\web\View */
 /* @var $model backend\models\DirectorProyectoPorProyecto */
 /* @var $form yii\widgets\ActiveForm */
-?>
 
+?>
 <div class="director-proyecto-por-proyecto-form">
-    <?php $form = ActiveForm::begin(); ?>
-    <?=
-    $form->field($model, 'iddirector_proyecto')->dropDownList(
-            ArrayHelper::map(DirectorProyecto::find()->all(), 'iddirector_proyecto', 'nombre'), ['prompt' => 'seleccione el director']
-    )
-    ?>
-    <?=
-    $form->field($model, 'idproyecto')->dropDownList(
-            ArrayHelper::map(Proyecto::find()->all(), 'idproyecto', 'nombre'), ['prompt' => 'seleccione el Proyecto']
-    )
-    ?>
-    <div class="form-group">
+    <div class="panel-body">
+        <?php $form = ActiveForm::begin(); ?>
+        <?= $form->field($model, 'iddirector_proyecto')->dropDownList(ArrayHelper::map(DirectorProyecto::find()->orderBy(['nombre' => SORT_ASC])->all(), 'iddirector_proyecto', 'nombre'), ['prompt' => 'seleccione el director']); ?>
+        <?= $form->field($model, 'idproyecto')->dropDownList(ArrayHelper::map(Proyecto::find()->orderBy(['nombre' => SORT_ASC])->all(), 'idproyecto', 'nombre'), ['prompt' => 'seleccione el Proyecto']);?>
+    </div>
+    <div class="panel-footer">
         <?= Html::submitButton($model->isNewRecord ? 'Asignar' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-primary' : 'btn btn-primary']) ?>
     </div>
     <?php ActiveForm::end(); ?>
--- frontend/views/director-proyecto-por-proyecto/create.php
+++ frontend/views/director-proyecto-por-proyecto/create.php
@@ -9,12 +9,15 @@ $this->title                   = 'Director Por Proyecto';
 $this->params['breadcrumbs'][] = ['label' => 'Director Proyecto Por Proyectos', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-
 <div class="director-proyecto-por-proyecto-create">
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?=
-    $this->render('_form', [
-        'model' => $model,
-    ])
-    ?>
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <?= Html::encode($this->title) ?>
+        </div>
+        <?=
+        $this->render('_form', [
+            'model' => $model,
+        ])
+        ?>
+    </div>
 </div>
