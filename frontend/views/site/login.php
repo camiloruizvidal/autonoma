b6320ca4980e7ba7781a8b76d5fa075515daa03f
camiloruizvidal <camiloruizvidal@gmail.com>
2018-01-17 11:10:53 -0500

    #065

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -9,23 +9,35 @@ use yii\bootstrap\ActiveForm;
 $this->title                   = 'Inicio de Sesión';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<link href="./css/login.css?v=<?php echo date('Ymdihs');?>" rel="stylesheet"/>
+<link href="./css/login.css?v=<?php echo date('Ymdihs'); ?>" rel="stylesheet"/>
 <div class = "container">
     <div class="wrapper">
-        <form id="login_form" method="post" name="Login_Form" class="form-signin">       
+        <form id="login_form" method="post" name="Login_Form" class="form-signin"> 
             <?php $form                          = ActiveForm::begin(['id' => 'login-form']); ?>
-            <h3 class="form-signin-heading">Bienvenido, por favor inicie sesión</h3>
-            <center><img src="imagen/autonoma.png" class="img-responsive" width="60%"></center>
+            <table>
+                <tr>
+                    <td>
+                        <h3 class="form-signin-heading">Bienvenido, por favor inicie sesión</h3>
+                <center><img src="imagen/autonoma.png" class="img-responsive" width="60%"></center>
+                </td>
+                <td>
+                    <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+                    <?= $form->field($model, 'password')->passwordInput() ?>
+                    <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                    <div class="form-group">
+                        <?= Html::submitButton('<i class="fa fa-unlock-alt" aria-hidden="true"></i> Ingresar', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                    </div>
+                    <div style="color:#999;margin:1em 0">
+                        <?= Html::a('¿Olvidaste su Contraseña ?', ['site/request-password-reset']) ?>.
+                    </div>
+
+                </td>
+                </tr>
+
+            </table>
+
             <hr class="colorgraph"><br>
-            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-            <?= $form->field($model, 'password')->passwordInput() ?>
-            <?= $form->field($model, 'rememberMe')->checkbox() ?>
-            <div style="color:#999;margin:1em 0">
-                Olvidaste tu Contraseña ? <?= Html::a('Click Aquí', ['site/request-password-reset']) ?>.
-            </div>
-            <div class="form-group">
-                <?= Html::submitButton('Ingresar', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-            </div>
+
 
 
             <?php ActiveForm::end(); ?>
